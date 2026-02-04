61f62839f2b28ed05d361a7c1d4998e2faa46ae0
maximkrouk <id.maximkrouk@gmail.com>
2026-02-04 10:22:56 +0100

    feat: Update tests

--- Tests/InterceptionTests/InterceptionTests.swift
+++ Tests/InterceptionTests/InterceptionTests.swift
@@ -1,169 +1,225 @@
-import XCTest
+import Testing
+import Foundation
 @testable import Interception
 
-final class InterceptionTests: XCTestCase {
-	func testNoInputWithOutput() {
-		let object = Object()
-		var _args: [Void] = []
-		var _outputs: [Int] = []
-		var _expectedOutputs: [Int] = []
-		var _count = 0
+@Suite("InterceptionTests")
+struct InterceptionTests {
+	@Suite("NoArgs")
+	struct NoArgsTests {
+		class Object: NSObject {
+			@objc dynamic
+			func void() { }
 
-		object.setInterceptionHandler(for: _makeMethodSelector(
-			selector: #selector(Object.zero),
-			signature: Object.zero
-		)) { result in
-			_args.append(result.args)
-			_outputs.append(result.output)
-			_count += 1
+			@discardableResult
+			@objc dynamic
+			func zero() -> Int { 0 }
 		}
 
-		let runs = 3
-		for _ in 0..<runs {
-			_expectedOutputs.append(object.zero())
+		@Test
+		func noOutput() async throws {
+			let object = Object()
+
+			var _interceptedArgs: [Void] = []
+			var _interceptedOutputs: [Void] = []
+			var _interceptionsCount = 0
+			var _actualOutputs: [Void] = []
+
+			object.setInterceptionHandler(for: _makeMethodSelector(
+				selector: #selector(Object.void),
+				signature: Object.void
+			)) { result in
+				_interceptionsCount += 1
+				_interceptedArgs.append(result.args)
+				_interceptedOutputs.append(result.output)
+			}
+
+			let runs = 3
+			for _ in 0..<runs {
+				_actualOutputs.append(object.void())
+			}
+
+			#expect(_interceptionsCount == runs)
+			#expect(_interceptedArgs.count == runs)
+			#expect(_interceptedOutputs.count == _actualOutputs.count)
 		}
 
-		XCTAssertEqual(_count, runs)
-		XCTAssertEqual(_args.count, runs)
-		XCTAssertEqual(_outputs, _expectedOutputs)
+		@Test
+		func simpleOutput() async throws {
+			let object = Object()
+
+			var _interceptedArgs: [Void] = []
+			var _interceptedOutputs: [Int] = []
+			var _interceptionsCount = 0
+			var _actualOutputs: [Int] = []
+
+			object.setInterceptionHandler(for: _makeMethodSelector(
+				selector: #selector(Object.zero),
+				signature: Object.zero
+			)) { result in
+				_interceptionsCount += 1
+				_interceptedArgs.append(result.args)
+				_interceptedOutputs.append(result.output)
+			}
+
+			let runs = 3
+			for _ in 0..<runs {
+				_actualOutputs.append(object.zero())
+			}
+
+			#expect(_interceptionsCount == runs)
+			#expect(_interceptedArgs.count == runs)
+			#expect(_interceptedOutputs.count == _actualOutputs.count)
+		}
 	}
 
-	func testInputWithNoOutput() {
-		let object = Object()
-		var _args: [Int] = []
-		var _outputs: [Void] = []
-		var _count = 0
+	@Suite("WithArgs")
+	struct WithArgsTests {
+		class Object: NSObject {
+			@objc dynamic
+			func discard(_ value: Int) {}
 
-		object.setInterceptionHandler(for: _makeMethodSelector(
-			selector: #selector(Object.discard),
-			signature: Object.discard
-		)) { result in
-			_args.append(result.args)
-			_outputs.append(result.output)
-			_count += 1
-		}
+			@discardableResult
+			@objc dynamic
+			func booleanForInteger(_ value: Int) -> Bool {
+				value != 0
+			}
 
-		let runs = 3
-		for i in 0..<runs { object.discard(i) }
+			@objc dynamic
+			func booleanAndForTwoIntegers(_ first: Int, _ second: Int) -> Bool {
+				booleanForInteger(first) && booleanForInteger(second)
+			}
 
-		XCTAssertEqual(_count, runs)
-		XCTAssertEqual(_args, .init(0..<runs))
-		XCTAssertEqual(_outputs.count, runs)
-	}
-
-	func testInputWithOutput() {
-		let object = Object()
-		var _args: [Int] = []
-		var _outputs: [Bool] = []
-		var _expectedOutputs: [Bool] = []
-		var _count = 0
-
-		object.setInterceptionHandler(for: _makeMethodSelector(
-			selector: #selector(Object.booleanForInteger),
-			signature: Object.booleanForInteger
-		)) { result in
-			_args.append(result.args)
-			_outputs.append(result.output)
-			_count += 1
-		}
-
-		let runs = 3
-		for i in 0..<runs {
-			_expectedOutputs.append(object.booleanForInteger(i))
-		}
-
-		XCTAssertEqual(_count, runs)
-		XCTAssertEqual(_args, .init(0..<runs))
-		XCTAssertEqual(_outputs, _expectedOutputs)
-	}
-
-	func testSimpleMultipleInputsWithOutput() {
-		let object = Object()
-		var _args: [(Int, Int)] = []
-		var _outputs: [Bool] = []
-		var _expectedArgs: [(Int, Int)] = []
-		var _expectedOutputs: [Bool] = []
-		var _count = 0
-
-		object.setInterceptionHandler(for: _makeMethodSelector(
-			selector: #selector(Object.booleanAndForTwoIntegers),
-			signature: Object.booleanAndForTwoIntegers
-		)) { result in
-			_args.append(result.args)
-			_outputs.append(result.output)
-			_count += 1
-		}
-
-		let outerRuns = 3
-		let innerRuns = 3
-		let runs = outerRuns * innerRuns
-		for i in 0..<outerRuns {
-			for j in 0..<innerRuns {
-				_expectedArgs.append((i, j))
-				_expectedOutputs.append(object.booleanAndForTwoIntegers(i, j))
+			@objc dynamic
+			func toString(_ first: Int, _ second: Bool) -> String {
+				String(describing: (first, second))
 			}
 		}
 
-		XCTAssertEqual(_count, runs)
-		XCTAssertEqual(_args.map(Pair.init), _expectedArgs.map(Pair.init))
-		XCTAssertEqual(_outputs, _expectedOutputs)
-	}
+		@Test
+		func noOutput() async throws {
+			let object = Object()
 
-	func testMultipleInputsWithOutput() {
-		let object = Object()
-		var _args: [(Int, Bool)] = []
-		var _outputs: [String] = []
-		var _expectedArgs: [(Int, Bool)] = []
-		var _expectedOutputs: [String] = []
-		var _count = 0
+			var _interceptedArgs: [Int] = []
+			var _interceptedOutputs: [Void] = []
+			var _interceptionsCount = 0
+			var _actualOutputs: [Void] = []
 
-		object.setInterceptionHandler(for: _makeMethodSelector(
-			selector: #selector(Object.toString),
-			signature: Object.toString
-		)) { result in
-			_args.append(result.args)
-			_outputs.append(result.output)
-			_count += 1
+			object.setInterceptionHandler(for: _makeMethodSelector(
+				selector: #selector(Object.discard),
+				signature: Object.discard
+			)) { result in
+				_interceptionsCount += 1
+				_interceptedArgs.append(result.args)
+				_interceptedOutputs.append(result.output)
+			}
+
+			let runs = 3
+			for i in 0..<runs {
+				_actualOutputs.append(object.discard(i))
+			}
+
+			#expect(_interceptionsCount == runs)
+			#expect(_interceptedArgs == .init(0..<runs))
+			#expect(_actualOutputs.count == _interceptedOutputs.count)
 		}
 
-		let outerRuns = 3
-		let innerRuns = 3
-		let runs = outerRuns * innerRuns
-		for i in 0..<outerRuns {
-			for j in 0..<innerRuns {
-				_expectedArgs.append((i, j.isMultiple(of: 2)))
-				_expectedOutputs.append(object.toString(i, j.isMultiple(of: 2)))
+		@Test
+		func simpleOutput() async throws {
+			let object = Object()
+
+			var _interceptedArgs: [Int] = []
+			var _interceptedOutputs: [Bool] = []
+			var _interceptionsCount = 0
+			var _actualOutputs: [Bool] = []
+
+			object.setInterceptionHandler(for: _makeMethodSelector(
+				selector: #selector(Object.booleanForInteger),
+				signature: Object.booleanForInteger
+			)) { result in
+				_interceptionsCount += 1
+				_interceptedArgs.append(result.args)
+				_interceptedOutputs.append(result.output)
+			}
+
+			let runs = 3
+			for i in 0..<runs {
+				_actualOutputs.append(object.booleanForInteger(i))
+			}
+
+			#expect(_interceptionsCount == runs)
+			#expect(_interceptedArgs == .init(0..<runs))
+			#expect(_interceptedOutputs == _actualOutputs)
+		}
+
+		func multipleOutputsSimpleOutput() async throws {
+			do { // same type
+				let object = Object()
+
+				var _interceptedArgs: [(Int, Int)]  = []
+				var _interceptedOutputs: [Bool] = []
+				var _interceptionsCount = 0
+
+				var _actualArgs: [(Int, Int)]  = []
+				var _actualOutputs: [Bool] = []
+
+				object.setInterceptionHandler(for: _makeMethodSelector(
+					selector: #selector(Object.booleanAndForTwoIntegers),
+					signature: Object.booleanAndForTwoIntegers
+				)) { result in
+					_interceptionsCount += 1
+					_interceptedArgs.append(result.args)
+					_interceptedOutputs.append(result.output)
+				}
+
+				let outerRuns = 3
+				let innerRuns = 3
+				let runs = outerRuns * innerRuns
+				for i in 0..<outerRuns {
+					for j in 0..<innerRuns {
+						_actualArgs.append((i, j))
+						_actualOutputs.append(object.booleanAndForTwoIntegers(i, j))
+					}
+				}
+
+				#expect(_interceptionsCount == runs)
+				#expect(_interceptedArgs.map(Pair.init) == _actualArgs.map(Pair.init))
+				#expect(_interceptedOutputs == _actualOutputs)
+			}
+
+			do { // different types
+				let object = Object()
+
+				var _interceptedArgs: [(Int, Bool)]  = []
+				var _interceptedOutputs: [String] = []
+				var _interceptionsCount = 0
+
+				var _actualArgs: [(Int, Bool)]  = []
+				var _actualOutputs: [String] = []
+
+				object.setInterceptionHandler(for: _makeMethodSelector(
+					selector: #selector(Object.toString),
+					signature: Object.toString
+				)) { result in
+					_interceptionsCount += 1
+					_interceptedArgs.append(result.args)
+					_interceptedOutputs.append(result.output)
+				}
+
+				let outerRuns = 3
+				let innerRuns = 3
+				let runs = outerRuns * innerRuns
+				for i in 0..<outerRuns {
+					for j in 0..<innerRuns {
+						_actualArgs.append((i, j.isMultiple(of: 2)))
+						_actualOutputs.append(object.toString(i, j.isMultiple(of: 2)))
+					}
+				}
+
+				#expect(_interceptionsCount == runs)
+				#expect(_interceptedArgs.map(Pair.init) == _actualArgs.map(Pair.init))
+				#expect(_interceptedOutputs == _actualOutputs)
 			}
 		}
-
-		XCTAssertEqual(_count, runs)
-		XCTAssertEqual(_args.map(Pair.init), _expectedArgs.map(Pair.init))
-		XCTAssertEqual(_outputs, _expectedOutputs)
-	}
-}
-
-fileprivate final class Object: NSObject, Sendable {
-	@discardableResult
-	@objc dynamic
-	func zero() -> Int { 0 }
-
-	@objc dynamic
-	func discard(_ value: Int) {}
-
-	@discardableResult
-	@objc dynamic
-	func booleanForInteger(_ value: Int) -> Bool {
-		value != 0
-	}
-
-	@objc dynamic
-	func booleanAndForTwoIntegers(_ first: Int, _ second: Int) -> Bool {
-		booleanForInteger(first) && booleanForInteger(second)
-	}
-
-	@objc dynamic
-	func toString(_ first: Int, _ second: Bool) -> String {
-		String(describing: (first, second))
 	}
 }
 
